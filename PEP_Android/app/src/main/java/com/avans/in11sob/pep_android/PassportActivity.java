0cc3c0233a959aee3fa4007c91ac35cc2f2804f9
Michael Schouten <manschouten@hotmail.com>
2016-03-15 22:56:04 +0100

    basic android structure
    basic navigatie is gemaakt zoals mockups

--- PEP_Android/app/src/main/java/com/avans/in11sob/pep_android/PassportActivity.java
+++ PEP_Android/app/src/main/java/com/avans/in11sob/pep_android/PassportActivity.java
@@ -5,27 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class PassportActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_passport);
     }
 
     public void buttonClick(View view){
         Intent intent;
         switch (view.getId()){
-            case R.id.scanner:
-                intent = new Intent(this, ScannerActivity.class);
+            case R.id.stijl:
+                intent = new Intent(this, StijlActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.passport:
-                intent = new Intent(this, PassportActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.settings:
-                intent = new Intent(this, SettingsActivity.class);
+            case R.id.kleurenPalet:
+                intent = new Intent(this, KleurenPaletActivity.class);
                 startActivity(intent);
                 break;
             default:
---
+++ PEP_Android/app/src/main/java/com/avans/in11sob/pep_android/StijlActivity.java
@@ -0,0 +1,13 @@
+package com.avans.in11sob.pep_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StijlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stijl);
+    }
+}
