9c152d2d1a3acd7cc30386a4c133f6199953cf48
Michael Schouten <manschouten@hotmail.com>
2016-03-23 14:22:52 +0100

    main page/ menu layout changed to design

--- PEP_Android/app/src/main/java/com/avans/in11sob/pep_android/Activity/SplashActivity.java
+++ PEP_Android/app/src/main/java/com/avans/in11sob/pep_android/Activity/SplashActivity.java
@@ -3,6 +3,8 @@ package com.avans.in11sob.pep_android.Activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 import com.avans.in11sob.pep_android.R;
 
 public class SplashActivity extends Activity {
@@ -12,29 +14,14 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        /****** Create Thread that will sleep for 1 seconds *************/
-        Thread background = new Thread() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
             public void run() {
-
-                try {
-                    // Thread will sleep for 1 seconds
-                    sleep(100000);
-
-                    // After 1 seconds redirect to another intent
-                    Intent intent = new Intent(getBaseContext(),MainActivity.class);
-                    startActivity(intent);
-
-                    //Remove activity
-                    finish();
-
-                } catch (Exception e) {
-
-                }
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
             }
-        };
-
-        // start thread
-        background.start();
+        }, 1000);
     }
 
     @Override
