fc5332dd727f1ba8ad3dee703ffda578d47abefd
Michael Schouten <manschouten@hotmail.com>
2016-06-20 14:07:30 +0200

    opencv shows progress

--- PEP_Android/app/src/main/java/com/avans/in11sob/pep_android/Activity/ScannerActivity.java
+++ PEP_Android/app/src/main/java/com/avans/in11sob/pep_android/Activity/ScannerActivity.java
@@ -16,8 +16,13 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ScannerActivity extends Activity implements CvCameraViewListener2 {
     private final static String TAG = "PEP::Scanner";
+    private static int WindowWidth;
+    private static int WindowHeight;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -61,16 +66,52 @@ public class ScannerActivity extends Activity implements CvCameraViewListener2 {
         }
     }
 
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
     public void onDestroy() {
         super.onDestroy();
         if(mOpenCvCameraView != null) {
             mOpenCvCameraView.disableView();
+            rowCol.clear();
         }
     }
 
     Mat newMat;
     @Override
     public void onCameraViewStarted(int width, int height) {
+        Log.e("BREEDTE:: ", String.valueOf(width));Log.e("HOOGTE:: ",String.valueOf(height));
+        WindowWidth = width;
+        WindowHeight = height;
+        // TL   TC  TR
+        //              TL::    ROW*.25, COL*.25    300*250
+        //              TC::    ROW*.50, COL*.25    600*250
+        //              TR::    ROW*.75, COL*.25    900*250
+        // CL   CC  CR
+        //              CR::    ROW*.25, COL*.50    300*500
+        //              CC::    ROW*.50, COL*.50    600*500
+        //              CL::    ROW*.75, COL*.50    900*500
+        // BL   BC  BR
+        //              BL::    ROW*.25, COL*.75    300*750
+        //              BC::    ROW*.50, COL*.75    600*750
+        //              BR::    ROW*.75, COL*.75    600*750
+        rowCol.put((WindowWidth*0.25),(WindowHeight*0.25));
+        rowCol.put((WindowWidth*0.50),(WindowHeight*0.25));
+        rowCol.put((WindowWidth*0.75),(WindowHeight*0.25));
+
+        rowCol.put((WindowWidth*0.25),(WindowHeight*0.50));
+        rowCol.put((WindowWidth*0.50),(WindowHeight*0.50));
+        rowCol.put((WindowWidth*0.75),(WindowHeight*0.50));
+
+        rowCol.put((WindowWidth*0.25),(WindowHeight*0.75));
+        rowCol.put((WindowWidth*0.50),(WindowHeight*0.75));
+        rowCol.put((WindowWidth*0.75),(WindowHeight*0.75));
+
         newMat = new Mat();
     }
 
@@ -78,9 +119,30 @@ public class ScannerActivity extends Activity implements CvCameraViewListener2 {
     public void onCameraViewStopped() {
         newMat.release();
     }
-
+    Map<Double, Double>rowCol = new HashMap<Double, Double>();
     @Override
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+        int rows = inputFrame.rgba().rows();
+        int clms = inputFrame.rgba().cols();
+
+//        Log.e("PRINTING RGB ROW:: ", String.valueOf(inputFrame.rgba().rows())); // HOOGTE
+//        Log.e("PRINTING RGB COL:: ", String.valueOf(inputFrame.rgba().cols())); // WIDTH
+        double[] data = inputFrame.rgba().get((rows/2), (clms/2));
+//        for (Map.Entry<Double, Double> entry : rowCol.entrySet()) {
+//            double[]data = inputFrame.rgba().get(entry.getKey().intValue(), entry.getValue().intValue());
+//            data[0]; // R
+//            data[1]; // G
+//            data[2]; // B
+//        }
+        compareColor((int)data[0], (int)data[1], (int)data[2]);
+//        Log.e("data 0::", String.valueOf(data[0]));
+//        Log.e("data 1::", String.valueOf(data[1]));
+//        Log.e("data 2::", String.valueOf(data[2]));
+
         return inputFrame.rgba();
+    }
+
+    public void compareColor(int R, int G, int B) {
+
     }
 }
